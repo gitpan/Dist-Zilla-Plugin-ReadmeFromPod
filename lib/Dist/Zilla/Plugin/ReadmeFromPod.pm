e770938d9008c5ce499ba691daa83e58d66d0054
Ævar Arnfjörð Bjarmason <avar@cpan.org>
2010-03-11 15:47:09 +0000

    Import of AVAR/Dist-Zilla-Plugin-ReadmeFromPod-0.06 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-ReadmeFromPod
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         AVAR/Dist-Zilla-Plugin-ReadmeFromPod-0.06.tar.gz
    gitpan-cpan-author:       AVAR
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/ReadmeFromPod.pm
+++ lib/Dist/Zilla/Plugin/ReadmeFromPod.pm
@@ -1,10 +1,12 @@
 package Dist::Zilla::Plugin::ReadmeFromPod;
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 # ABSTRACT: Automatically convert POD to a README for Dist::Zilla
 
 use Moose;
 use Moose::Autobox;
+use IO::Handle;
+use File::Temp qw< tempdir tempfile >;
 
 #with 'Dist::Zilla::Role::FileGatherer';
 with 'Dist::Zilla::Role::InstallTool';    # after PodWeaver
@@ -14,7 +16,8 @@ sub setup_installer {
 
     require Dist::Zilla::File::InMemory;
 
-    open( my $out_fh, ">", \my $content );
+    my $dir = tempdir( CLEANUP => 1 );
+    my ( $out_fh, $filename ) = tempfile( DIR => $dir );
 
     my $mmcontent = $self->zilla->main_module->content;
 
@@ -22,6 +25,15 @@ sub setup_installer {
     my $parser = Pod::Text->new();
     $parser->output_fh($out_fh);
     $parser->parse_string_document($mmcontent);
+
+    $out_fh->sync();
+    close $out_fh;
+
+    # Do *not* convert this to something that doesn't use open() for
+    # cleverness, that breaks UTF-8 pod files.
+    open( my $fh, "<", $filename ) or die "Can't open file '$filename'";
+    my $content = do { local $/; <$fh> };
+    close $fh;
 
     my $file =
       $self->zilla->files->grep( sub { $_->name =~ m{README\z} } )->head;
@@ -57,7 +69,7 @@ Dist::Zilla::Plugin::ReadmeFromPod - Automatically convert POD to a README for D
 
 =head1 VERSION
 
-version 0.05
+version 0.06
 
 =head1 SYNOPSIS
 
@@ -79,6 +91,6 @@ The code is mostly a copy-paste of L<Module::Install::ReadmeFromPod>
 This software is copyright (c) 2010 by Fayland Lam.
 
 This is free software; you can redistribute it and/or modify it under
-the same terms as perl itself.
+the same terms as the Perl 5 programming language system itself.
 
 =cut
