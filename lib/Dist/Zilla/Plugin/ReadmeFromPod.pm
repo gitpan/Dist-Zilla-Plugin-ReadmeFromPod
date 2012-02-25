586f573054bd9fd7f5e7746c738bc4aa1db9491a
Fayland 林 <fayland@gmail.com>
2012-02-25 03:13:29 +0000

    Import of FAYLAND/Dist-Zilla-Plugin-ReadmeFromPod-0.06 from CPAN.
    gitpan-cpan-distribution: Dist-Zilla-Plugin-ReadmeFromPod
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         FAYLAND/Dist-Zilla-Plugin-ReadmeFromPod-0.06.tar.gz
    gitpan-cpan-author:       FAYLAND
    gitpan-cpan-maturity:     released

--- lib/Dist/Zilla/Plugin/ReadmeFromPod.pm
+++ lib/Dist/Zilla/Plugin/ReadmeFromPod.pm
@@ -1,79 +1,110 @@
 package Dist::Zilla::Plugin::ReadmeFromPod;
-BEGIN {
-  $Dist::Zilla::Plugin::ReadmeFromPod::AUTHORITY = 'cpan:AVAR';
+{
+    $Dist::Zilla::Plugin::ReadmeFromPod::VERSION = '0.06';
 }
-BEGIN {
-  $Dist::Zilla::Plugin::ReadmeFromPod::VERSION = '0.14';
-}
+
+# ABSTRACT: Automatically convert POD to a README for Dist::Zilla
 
 use Moose;
 use Moose::Autobox;
-use IO::Handle;
-use Encode qw( encode );
+with 'Dist::Zilla::Role::FileGatherer';
 
-with 'Dist::Zilla::Role::InstallTool';
+has filename => (
+    is      => 'ro',
+    isa     => 'Str',
+    lazy    => 1,
+    builder => '_build_filename',
+);
 
-sub setup_installer {
-  my ($self, $arg) = @_;
+sub _build_filename {
+    my $self = shift;
+    $self->zilla->main_module->name;
+}
 
-  require Dist::Zilla::File::InMemory;
-  
-  my $mmcontent = $self->zilla->main_module->content;
+sub gather_files {
+    my ( $self, $arg ) = @_;
 
-  require Pod::Text;
-  my $parser = Pod::Text->new();
-  $parser->output_string( \my $input_content );
-  $parser->parse_string_document( $mmcontent );
-  
-  my $content;
-  if( defined $parser->{encoding} ){ 
-    $content = encode( $parser->{encoding} , $input_content );
-  } else { 
-     $content = $input_content; 
-  }
+    require Dist::Zilla::File::FromCode;
 
-  my $file = $self->zilla->files->grep( sub { $_->name =~ m{README\z} } )->head;
+    my $file = Dist::Zilla::File::FromCode->new(
+        {
+            code => sub {
+                my $mmcontent = $self->zilla->files->grep(
+                    sub {
+                        $_->name eq $self->filename;
+                    }
+                )->head->content;
 
-  if ( $file ) {
-    $file->content( $content );
-    $self->zilla->log("Override README from [ReadmeFromPod]");
-  } else {
-    $file = Dist::Zilla::File::InMemory->new({
-        content => $content,
-        name    => 'README',
-    });
+                require Pod::Text;
+                my $parser = Pod::Text->new();
+                $parser->output_string( \my $input_content );
+                $parser->parse_string_document($mmcontent);
+
+                my $content;
+                if ( defined $parser->{encoding} ) {
+                    $content = encode( $parser->{encoding}, $input_content );
+                }
+                else {
+                    $content = $input_content;
+                }
+
+                return $content;
+            },
+            name => 'README',
+        }
+    );
     $self->add_file($file);
-  }
 
-  return;
+    return;
 }
 
 __PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+__END__
+
+=pod
 
 =head1 NAME
 
 Dist::Zilla::Plugin::ReadmeFromPod - Automatically convert POD to a README for Dist::Zilla
+
+=head1 VERSION
+
+version 0.06
 
 =head1 SYNOPSIS
 
     # dist.ini
     [ReadmeFromPod]
 
+    # or
+    [ReadmeFromPod]
+    filename = lib/XXX.pod
+
+    # to fix "[DZ] attempt to add README multiple times; added by: @Filter/Readme
+    [@Filter]
+    remove = Readme
+
+    [ReadmeFromPod]
+
 =head1 DESCRIPTION
 
-Generates a plain-text README for your L<Dist::Zilla> powered dist
-from its C<main_module> with L<Pod::Text>.
+generate the README from C<main_module> (or specified) by L<Pod::Text>
 
-=head1 AUTHORS
+The code is mostly a copy-paste of L<Module::Install::ReadmeFromPod>
 
-Fayland Lam <fayland@gmail.com> and E<AElig>var ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+=head1 AUTHOR
 
-=head1 LICENSE AND COPYRIGHT
+Fayland Lam <fayland@gmail.com>
 
-Copyright 2010 Fayland Lam <fayland@gmail.com> and E<AElig>var
-ArnfjE<ouml>rE<eth> Bjarmason <avar@cpan.org>
+=head1 COPYRIGHT AND LICENSE
 
-This program is free software, you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This software is copyright (c) 2012 by Fayland Lam.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
